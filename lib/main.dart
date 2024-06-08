f88356b3d8ed8d1ea13c30cceec6bceb132478e4
abdulmoiz15890 <abdulmoiz15890@gmail.com>
2024-06-08 19:53:30 +0000

    Task 2 Periodic table completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:myapp/task1_ludo.dart';
+import 'package:myapp/task2_periodic_table.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +13,15 @@ class MyApp extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Assignment 2',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ScreenUtilInit(
+      child: MaterialApp(
+        title: 'Assignment 2',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: PeriodicTable(),
       ),
-      home: LudoScreen(),
     );
   }
 }
